0cf0f0aed2e0cd54e53bceeecb75f1e30ea73032
robinson <robinpaulista85@gmail.com>
2021-04-26 17:22:48 -0300

    Operadores de atribuição compostos

---
+++ aula32_operadores_de_atribuicao_compostos.py
@@ -0,0 +1,33 @@
+''' Operadores de atribuição
+x = 9
+y = 3
++= -> (x += y) = 12
+-= -> (x -= y) = 6
+*= -> (x *= y) = 27
+/= -> (x /= y) = 3
+%= -. (x %= y) = 0'''
+
+x = 9
+y = 3
+x += y
+print(x)
+
+x = 9
+y = 3
+x -= y
+print(x)
+
+x = 9
+y = 3
+x *= y
+print(x)
+
+x = 9
+y = 3
+x /= y
+print(x)
+
+x = 9
+y = 3
+x %= y
+print(x)
