ecfc8adb47706afa5a63dd49b85a42b39d323a5c
robinson <robinpaulista85@gmail.com>
2021-04-26 10:43:20 -0300

    Operadores de atribuição

---
+++ aula30_operadores_de_atribuicao.py
@@ -0,0 +1,17 @@
+''' Operadores de atribuições - O valor ao aldo direito do operador é atribuido
+à variável a esquerda do operador. 
+
+nome = valor
+
+nome --> variável        valor --> valor a ser atribuído
+
+= atribui
+== iguala/compara
+
+atribuição - x = y
+comparação - x == y'''
+
+a = 9
+x = y = z = a
+
+x == y == z == a
